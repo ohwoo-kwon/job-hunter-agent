ccb940211aa53bc681e11e074eea24ef59047c7e
ohwoo_kwon <kwonow1@gmail.com>
2025-08-28 12:57:15 +0900

    add tool

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
 from crewai.project import CrewBase, task, agent, crew
+from models import JobList, RankedJobList, ChosenJob
+from tools import web_search_tool
 
 @CrewBase
 class JobHunterCrew:
@@ -11,55 +13,59 @@ class JobHunterCrew:
     @agent
     def job_search_agent(self):
         return Agent(
-            config=self.agents_config['job_search_agent']
+            config=self.agents_config['job_search_agent'],
+            tools=[web_search_tool],
         )
 
     @agent
     def job_matching_agent(self):
         return Agent(
-            config=self.agents_config['job_matching_agent']
+            config=self.agents_config['job_matching_agent'],
         )
 
     @agent
     def resume_optimization_agent(self):
         return Agent(
-            config=self.agents_config['resume_optimization_agent']
+            config=self.agents_config['resume_optimization_agent'],
         )
 
     @agent
     def company_research_agent(self):
         return Agent(
-            config=self.agents_config['company_research_agent']
+            config=self.agents_config['company_research_agent'],
         )
 
     @agent
     def interview_prep_agent(self):
         return Agent(
-            config=self.agents_config['interview_prep_agent']
+            config=self.agents_config['interview_prep_agent'],
         )
     
     @task
     def job_extraction_task(self):
         return Task(
-            config=self.tasks_config['job_extraction_task']
+            config=self.tasks_config['job_extraction_task'],
+            output_pydantic=JobList,
         )
     
     @task
     def job_matching_task(self):
         return Task(
-            config=self.tasks_config['job_matching_task']
+            config=self.tasks_config['job_matching_task'],
+            output_pydantic=RankedJobList,
         )
     
     @task
     def job_selection_task(self):
         return Task(
-            config=self.tasks_config['job_selection_task']
+            config=self.tasks_config['job_selection_task'],
+            output_pydantic=ChosenJob,
         )
     
     @task
     def resume_rewriting_task(self):
         return Task(
-            config=self.tasks_config['resume_rewriting_task']
+            config=self.tasks_config['resume_rewriting_task'],
         )
     
     @task
@@ -68,7 +74,7 @@ class JobHunterCrew:
             config=self.tasks_config['company_research_task'],
             context=[
                 self.job_selection_task(),
-            ]
+            ],
         )
     
     @task
@@ -79,7 +85,7 @@ class JobHunterCrew:
                 self.job_selection_task(),
                 self.resume_rewriting_task(),
                 self.company_research_task(),
-            ]
+            ],
         )
 
     @crew
